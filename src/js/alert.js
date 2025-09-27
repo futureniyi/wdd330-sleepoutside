8ddaa154ce84c2a8726e6e365dc688fb1d41bf9c
futureniyi <futureniyi@yahoo.com>
2025-09-27 16:42:27 +0100

    Add customizable alert to index.html

---
+++ src/js/alert.js
@@ -0,0 +1,38 @@
+// import { getLocalStorage } from "./utils.mjs";
+
+export default class Alert {
+  constructor(alertsPath) {
+    this.alertsPath = alertsPath;
+  }
+
+  async init() {
+    const alerts = await this.getAlerts();
+    if (alerts.length > 0) {
+      this.renderAlerts(alerts);
+    }
+  }
+
+  async getAlerts() {
+    const response = await fetch(this.alertsPath);
+    if (!response.ok) {
+      throw new Error("Failed to fetch alerts");
+    }
+    return response.json();
+  }
+
+  renderAlerts(alerts) {
+    const alertList = document.createElement("section");
+    alertList.className = "alert-list";
+
+    alerts.forEach((alert) => {
+      const alertElement = document.createElement("p");
+      alertElement.textContent = alert.message;
+      alertElement.style.backgroundColor = alert.background;
+      alertElement.style.color = alert.color;
+      alertList.appendChild(alertElement);
+    });
+
+    const mainElement = document.querySelector("main");
+    mainElement.prepend(alertList);
+  }
+}
